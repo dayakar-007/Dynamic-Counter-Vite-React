df97a731210dd4a1c55c80ef2785b44c65a6f2a0
Narala Dayakar Reddy <naraladayakar072@gmail.com>
2024-08-02 18:07:10 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Counter from "./Components/Counter"
+
+
+function App() {  
+
+  return (
+    <>
+    <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Counter.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export default function Counter() {
+    const [count,setCount]=useState(0);
+    const [change,setChange]=useState(0);
+    const decrementHandler=()=>{
+            setCount(count-change);
+        }
+    const resetHandler =()=>
+    {
+        setCount(0);
+        setChange(0);
+    }
+    const incrementHandler =()=>
+    {
+        setCount(count+Number(change));
+    }
+    const valueOnchange =(e)=>
+    {
+        setChange(e.target.value); 
+    }
+  return (
+    <div className='counter-container'>
+      <p className='counter-display'>{count}</p>
+      <button className='counter-button' onClick={decrementHandler}>{change<=-1||change>=1?`Decrement by ${change}`:`Decrement`}</button>
+      <button className='counter-button' onClick={resetHandler}>Reset</button>
+      <button className='counter-button' onClick={incrementHandler}>{change<=-1||change>=1?`Increment by ${change}`:`Increment`}</button><br></br>
+      <form action="">
+         <input className='counter-change' value={change} onChange={valueOnchange} type='number'/>
+      </form>
+      
+     
+    </div>
+  )
+}
